c22704d1545187c4d3bc482d0b2d496f095cf5ce
non-97 <nonmonaka07@gmail.com>
2023-11-13 14:20:48 +0900

    Initial commit

---
+++ test/config-rule.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ConfigRule from '../lib/config-rule-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/config-rule-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ConfigRule.ConfigRuleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
